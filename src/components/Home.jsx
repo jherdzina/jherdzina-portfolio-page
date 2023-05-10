cdb19f9f865a3d71ec344c37f3d7fe073b00a7b4
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2023-05-10 18:26:50 -0500

    first commit, portfolio page w/ nav

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroImage from '../assets/heroImage.png';
+import { FaArrowCircleDown } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Home
